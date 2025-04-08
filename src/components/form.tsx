f75b4991a89c2cb783f16af1aa1cd48f686d2501
Alexferraz2 <alex_ferraz2@hotmail.com>
2025-04-07 23:42:09 -0300

    Adcionado upload de arquivo via dropzone

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,12 +1,15 @@
 "use client"
 
 import axios from "axios"
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, useEffect, useState } from "react"
+import { useDropzone } from "react-dropzone"
+
 
 export const Form = () => {
     const [selectedFile, setSelectedFile] = useState<File>()
     const [legendField, setLegendField] = useState('')
     const [progressUpload, setProgressUpload] = useState(0)
+    const { acceptedFiles, getRootProps, getInputProps} = useDropzone()
 
     const HandleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
         if(e.target.files && e.target.files.length > 0) {
@@ -39,10 +42,40 @@ export const Form = () => {
             console.log(reqi.data)
         }
     }
+    
+    const HandleDropZone = async () => {
+        const formData = new FormData()
+            formData.append('file', acceptedFiles[0])
+            formData.append('legend', legendField)
 
+            const url = 'https://b7web.com.br/uploadtest/'
 
+            const reqi = await axios.post(url, formData, {
+                onUploadProgress: (progressEvent) => {
+                    if(progressEvent.total) {
+                        const pct = Math.floor((progressEvent.loaded / progressEvent.total ) * 100)
+                        setProgressUpload(pct)
+                        console.log(pct)
+                    }
+                } 
+            });
+    }
+    useEffect(() => {
+        if(acceptedFiles.length > 0 ) {
+            setSelectedFile(acceptedFiles[0]) 
+            HandleDropZone()
+        }
+    }, [acceptedFiles])
     return(
         <div>
+            <div className="bg-gray-400 p-5 h-60 flex justify-center items-center" {...getRootProps()}>
+                <input {...getInputProps()}/>
+                <p className="text-white text-3xl">Arraste e solte o arquivo aqui, para fazer o upload!</p>
+            </div>
+            <div>
+                Arquivos: {acceptedFiles.length}
+            </div>
+
             <input 
                 type="file" 
                 className="block my-3"
